3537d9a521b253d3b4f92ba99ae84f04e626c98f
ColinKintzinger <majic11300@gmail.com>
2024-02-21 21:40:27 -0600

    location code added and started movement of melee circle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,13 @@
  * CHANGE LOG
  * Dylan - 02/19/24 - Added onCollisionEnter2D function to get scene transitions working
  * Dylan - 02/21/24 - Refactored scene transtion to a door script in SceneTransition
+ * Colin - 02/21/24 - added variables and some lines of code in the update for finding length between player and curser 
  */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -18,26 +20,35 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float verticalInput;
     public float speed = 3.0f;
-    public Vector3 mousePos;
-    public Vector3 target;
-    public float angle;
-    public GameObject player;
 
+    private Vector3 mousePos;
+    public Vector3 target;
+    public Vector3 worldPos;
+    public float angle;
+    public Vector3 playerPos;
+
+    public GameObject meleeCircle;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //meleeCircle= GetComponent<GameObject>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //gets position of mouse and player for melee attack location
         mousePos = Input.mousePosition;
-        target= mousePos - transform.position;
-        angle= Vector2.Angle(mousePos, transform.forward);
-        Debug.Log("Trans.pos:" + transform.position);
-
+        worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        playerPos = transform.position;
+        target = worldPos - playerPos;
+        //target= target.normalized;
+        angle= (Mathf.Atan(target.y/target.x)*180/Mathf.PI);
+        //Debug.Log("Trans.pos:" + transform.position);
+        
+        meleeCircle.transform.position = new Vector3(target.x,target.y, 0);
+        meleeCircle.transform.rotation = new Quaternion(0, 0 , angle, 0).normalized;
 
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
