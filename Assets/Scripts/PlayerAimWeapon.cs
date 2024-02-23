91bc977fa730dda565abccfb6480be48e0746e69
ColinKintzinger <majic11300@gmail.com>
2024-02-22 19:33:56 -0600

    melee appear and diappear plus rotation

--- Assets/Scripts/PlayerAimWeapon.cs
+++ Assets/Scripts/PlayerAimWeapon.cs
@@ -5,23 +5,40 @@ using UnityEngine;
 public class PlayerAimWeapon : MonoBehaviour
 {
     private Transform aimTransform;
-
+    public GameObject meleeLine;
     private void Awake()
     {
         aimTransform = transform.Find("Aim");
+        
     }
 
     private void Update()
     {
         // needs to pull a reference to the mouses in world position
+        meleeAiming();
+        meleAttack();
+    }
+    private void meleAttack()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("true");
+            meleeLine.gameObject.SetActive(true);
+        }
+        else
+        {
+            meleeLine.gameObject.SetActive(false);
+        }
+    }
+    private void meleeAiming()
+    {
         Vector3 mousePosition = GetMouseWorldPositon();
 
         Vector3 aimDirection = (mousePosition - transform.position).normalized;
         float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         aimTransform.eulerAngles = new Vector3(0, 0, angle);
-        Debug.Log(angle);
+        //Debug.Log(angle);
     }
-
 
     public static Vector3 GetMouseWorldPositon() {
         Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
