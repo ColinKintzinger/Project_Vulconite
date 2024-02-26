73a371a3905bedd580238829f7b0fb78400ad741
DylanRothbauer <djrothbauer@gmail.com>
2024-02-26 08:53:39 -0600

    Changed speed of A* enemy

--- Assets/Scripts/OutOfBounds.cs
+++ Assets/Scripts/OutOfBounds.cs
@@ -7,6 +7,7 @@
  * 
  * CHANGE LOG
  * Zach - 02/23/24 - Added comments.
+ * 
  */
 using System.Collections;
 using System.Collections.Generic;
@@ -18,6 +19,7 @@ public class OutOfBounds : MonoBehaviour
     public float upperBound = 6;
     public float lowerBound = -6;
     public float rightBound = 25;
+    
 
     // Start is called before the first frame update
     void Start()
