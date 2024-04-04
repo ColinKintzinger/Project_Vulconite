2fedf1e6c9f003a6c287b5c0befc8ec30ed4e8ef
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2024-04-04 15:04:19 -0500

    done with the collision problems

--- Assets/Scripts/Alpha/Enemy/EnemyBullet.cs
+++ Assets/Scripts/Alpha/Enemy/EnemyBullet.cs
@@ -38,4 +38,11 @@ public class EnemyBullet : MonoBehaviour
     {
         
     }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
