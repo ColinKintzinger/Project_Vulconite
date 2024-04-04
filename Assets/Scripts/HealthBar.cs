0d56be7057f911ec1188c6b93a5b830a9b7418cc
DylanRothbauer <djrothbauer@gmail.com>
2024-04-04 15:38:30 -0500

    Added bullet collider
    Need to fix to trigger and to work

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -10,7 +10,7 @@ public class HealthBar : MonoBehaviour
 
 
     public float startingHealth = 10;
-    public PlayerStats stats;
+    public PlayerStats playerStats;
 
     private float currentHealth = 10; //playerStats.health;
     private GameObject healthContainer;
@@ -20,7 +20,7 @@ public class HealthBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        stats = Resources.Load("PlayerStats") as PlayerStats;
+        playerStats = Resources.Load("PlayerStats") as PlayerStats;
         scaleAdjustment = transform.localScale.x / startingHealth;
         basePosition = transform.localPosition.x;
     }
@@ -29,6 +29,7 @@ public class HealthBar : MonoBehaviour
     void Update()
     {
         //currentHealth = playerStats.health;
+        Debug.Log(playerStats);
         transform.localScale = new Vector3(currentHealth * scaleAdjustment,transform.localScale.y, 0f) ;
         float positionXAdjustment = (currentHealth * scaleAdjustment - startingHealth * scaleAdjustment) / 2;
         transform.localPosition = new Vector3(basePosition+positionXAdjustment, 0, 1);
