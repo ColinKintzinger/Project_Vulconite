bcb90ca75ac86f0d5a629cdad984a1046311a5d8
Staton Otto <statonotto@gmail.com>
2024-04-08 11:20:13 -0500

    Updated the stationary enemy

---
+++ Assets/Scenes/Post-Beta/DamageVisualization.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageVisualization : MonoBehaviour
+{
+    public GameObject gameObject;
+    public string colorNorm = "FFFFFF";
+    public string colorDam = "FF3737";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.getComponent() = "FF3737";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
