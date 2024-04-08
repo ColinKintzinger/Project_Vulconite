85cb948419d9ed9e6426b46f2d25f08d37985112
Staton Otto <statonotto@gmail.com>
2024-04-08 13:39:12 -0500

    Finished the Damage Visualization Function
    Its for the artists to start picking damage colors for the characters.

--- Assets/Scenes/Post-Beta/DamageVisualization.cs
+++ Assets/Scenes/Post-Beta/DamageVisualization.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class DamageVisualization : MonoBehaviour
 {
-    public GameObject gameObject;
-    public string colorNorm = "FFFFFF";
-    public string colorDam = "FF3737";
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //gameObject.getComponent() = "FF3737";
-    }
+    public Color colorNorm = new Color(255 / 255f, 255 / 255f, 255 / 255f);
+    public Color colorDam = new Color(255/255f,55/255f,55/255f);
+    private float timer = 3;
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer = timer - Time.deltaTime*3;
+        if (timer <= 0)
+            timer = 3;
+        else if (timer <= 1)
+            gameObject.GetComponent<SpriteRenderer>().color = colorDam;
+        else if (timer <= 2)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = colorNorm;
+        }
+        Debug.Log("Timer = " + timer);
     }
 }
