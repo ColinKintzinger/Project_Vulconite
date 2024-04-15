c8d61035c9855d2a421f2c5d0a15f9e0d704d2b1
DylanRothbauer <djrothbauer@gmail.com>
2024-04-15 15:55:04 -0500

    Setting up text for charms

--- Assets/Scripts/DamageCharm.cs
+++ Assets/Scripts/DamageCharm.cs
@@ -8,15 +8,18 @@
  */
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class DamageCharm : Charm
 {
+    public GameObject pickupIndicatorPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //pickupIndicator.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,8 +30,20 @@ public class DamageCharm : Charm
     public override void ApplyBuff(PlayerStats playerStats)
     {
         //base.applyBuff();
-        float randomDamageBuff = Random.Range(0.25f, .75f);
+        float randomDamageBuff = Random.Range(0.05f, 0.15f);
         playerStats.damage += (playerStats.damage * randomDamageBuff);
+
         Debug.Log("Damage Charm Collected! Damage increased by " + (100 * randomDamageBuff) + "percent");
+        string text = "Damage Charm Collected!\n Damage increased by " + System.Math.Round(100 * randomDamageBuff, 2) + "%";
+
+        //pickupIndicator.GetComponent<TextMeshPro>().SetText(text);
+        //Instantiate(pickupIndicator, transform);
+
+        GameObject pickupIndicator = Instantiate(pickupIndicatorPrefab, transform.position, Quaternion.identity, transform);
+        pickupIndicator.GetComponent<TextMeshPro>().text = text;
+
+        Destroy(pickupIndicator, 3f);
+
     }
+
 }
