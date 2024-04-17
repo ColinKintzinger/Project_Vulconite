abc3e8b436b49fb6772376dee667c03ae1c5842a
DylanRothbauer <djrothbauer@gmail.com>
2024-04-17 15:54:41 -0500

    Got health charm implemented!
    We now have a health charm! Lets go!

---
+++ Assets/Scripts/HealthCharm.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCharm : Charm
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void ApplyBuff(PlayerStats playerStats)
+    {
+        //base.applyBuff();
+        float randomHealthBuff = Random.Range(0.05f, 0.15f);
+        playerStats.health += (playerStats.health * randomHealthBuff);
+
+        if (playerStats.health >= 100.00f)
+        {
+            playerStats.health = 100.00f;
+        }
+        //Debug.Log("Damage Charm Collected! Damage increased by " + (100 * randomDamageBuff) + "percent");
+        string text = "+" + System.Math.Round(100 * randomHealthBuff, 2) + "% Health";
+
+        ShowIndicator(text, this);
+    }
+}
