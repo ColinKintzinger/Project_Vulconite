6b0b164c8168ab6977aecb84e6ca224e4646222f
Staton Otto <statonotto@gmail.com>
2024-04-18 00:31:32 -0500

    Added a bunch of stuff
    Camera bounds script, enemy movement script update, and general purpose damage script is started and incomplete.

--- Assets/Scripts/Animation Scripts/Enemy Animation Director.cs
+++ Assets/Scripts/Animation Scripts/Enemy Animation Director.cs
@@ -1,9 +1,21 @@
+/* Animation Controller for the Enemies
+ * Staton Otto and Colin 
+ * Desc: Controls the enemy animation and displays movement for the enemy. (Most likely will need to 
+ * adjust the attack to go after the player rather than its movement for it to actually animate the 
+ * attack at the player).
+ * ---- CHANGELOG ----
+ * Staton Otto (2024/04/17) - Added a timer to allow the vectors to change between updates.
+ * 
+ * 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAnimationDirector : MonoBehaviour
 {
+    float timer = 0.01f;
     Vector3 newPosition = new Vector3(0, 0, 0);
     Vector3 oldPositon = new Vector3(0, 0, 0);
     bool isIdle = true;
@@ -17,18 +29,26 @@ public class EnemyAnimationDirector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        oldPositon = newPosition;
-        newPosition = transform.position;
-        if (newPosition == oldPositon)
+        if (timer <= 0)
         {
-            angle = -90f;
-            isIdle = true;
+            oldPositon = newPosition;
+            newPosition = transform.position;
+            if (newPosition == oldPositon)
+            {
+                angle = -90f;
+                isIdle = true;
+            }
+            else
+            {
+                Vector3 targetVector = newPosition - oldPositon;
+                angle = Mathf.Atan2(targetVector.y, targetVector.x) * Mathf.Rad2Deg;
+                isIdle = false;
+            }
+            Debug.Log("Target Angle= " + angle);
+            timer = 0.01f;
         }
         else {
-            Vector3 targetVector =  newPosition - oldPositon;
-            angle = Mathf.Atan2(targetVector.y, targetVector.x) * Mathf.Rad2Deg;
-            isIdle = false;
+            timer -= Time.deltaTime;
         }
-        Debug.Log("Target Angle= " + angle);
     }
 }
