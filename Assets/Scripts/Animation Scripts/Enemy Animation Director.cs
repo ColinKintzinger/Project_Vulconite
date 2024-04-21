e8dd2fb4d2963063befcfb9f056b3260c6e6a6d4
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2024-04-21 13:43:38 -0500

    ranged enemies animation (loading on other machine)

--- Assets/Scripts/Animation Scripts/Enemy Animation Director.cs
+++ Assets/Scripts/Animation Scripts/Enemy Animation Director.cs
@@ -20,11 +20,17 @@ public class EnemyAnimationDirector : MonoBehaviour
     Vector3 oldPositon = new Vector3(0, 0, 0);
     bool isIdle = true;
     float angle;
+    private Animator anim;
+    private Vector2 moveDir;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         newPosition = transform.position;
+        anim = GetComponent<Animator>();
+        rb= GetComponent<Rigidbody2D>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -44,11 +50,51 @@ public class EnemyAnimationDirector : MonoBehaviour
                 angle = Mathf.Atan2(targetVector.y, targetVector.x) * Mathf.Rad2Deg;
                 isIdle = false;
             }
-            Debug.Log("Target Angle= " + angle);
+           Debug.Log("Target Angle= " + angle);
             timer = 0.01f;
         }
         else {
             timer -= Time.deltaTime;
+        }
+        HandleMovement();
+
+    }
+    void HandleMovement()
+    {
+        float moveX = 0f;
+        float moveY=0f;
+         if(angle<135 && angle > 45)
+        {
+            moveY = +1f;
+            //Debug.Log("up");
+        }
+        else if (angle > -135 && angle < -45)
+        {
+            moveY = -1f;
+            //Debug.Log("down");
+        }
+        else if (angle < 45 &&  angle > -45)
+        {
+          moveX = +1f;
+            //Debug.Log("right");
+        }
+        else if (angle > 135 || angle < -135)
+        {
+            moveX = -1f;
+            //Debug.Log("left");
+        }
+        moveDir = new Vector2(moveX, moveY).normalized;
+        if (isIdle)
+        {
+            rb.velocity = Vector2.zero;
+
+
+        }
+        else
+        {
+            anim.SetFloat("Horizontal", moveDir.x);
+            anim.SetFloat("Verticle", moveDir.y);
+            
         }
     }
 }
