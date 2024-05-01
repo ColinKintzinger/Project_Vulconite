827830722b9fa74e544e244c18fbfa89683f853a
speckanz9788 <speckanz9788@my.uwstout.edu>
2024-05-01 15:50:23 -0500

    Arrows check if loot box exist

--- Assets/BobUpAndDown.cs
+++ Assets/BobUpAndDown.cs
@@ -8,13 +8,17 @@ public class BobUpAndDown : MonoBehaviour
     public AnimationCurve myCurve;
     public GameObject dependant1;
     public GameObject dependant2;
-    private bool destroyed = false;
     private GameObject lootBox;
+    private bool boxExists = false;
 
     // Start is called before the first frame update
     void Start()
     {
         lootBox = GameObject.FindGameObjectWithTag("LootBox");
+        if (lootBox != null)
+        {
+            boxExists = true;
+        }
     }
 
     // Update is called once per frame
@@ -22,9 +26,19 @@ public class BobUpAndDown : MonoBehaviour
     {
         transform.position = new Vector3(transform.position.x, myCurve.Evaluate((Time.time % myCurve.length)), transform.position.z);
 
-        if (dependant1 == null || dependant2 == null || !lootBox.GetComponent<BoxCollider2D>().enabled)
+        if (boxExists)
         {
-            gameObject.SetActive(false);
+            if (!lootBox.GetComponent<BoxCollider2D>().enabled)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            if (dependant1 == null || dependant2 == null)
+            {
+                gameObject.SetActive(false);
+            }
         }
     }
 }
